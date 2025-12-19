b9798529e5c13218a9b52ffe5e4f4c86efaf8414
JJJ4065 <darknessrisesgamer999@gmail.com>
2025-12-19 09:11:39 +0700

    setup be project based on mrPhucBienVan - fix

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
 
-class Settings:
-    SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL")
+# class Settings:
+#     SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-settings = Settings()
+# settings = Settings()
