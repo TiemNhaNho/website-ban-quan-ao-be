546eb933eedfc7aae1eeb1a544d88ecf3f86c186
JJJ4065 <darknessrisesgamer999@gmail.com>
2025-12-19 09:12:46 +0700

    setup be project based on mrPhucBienVan - fix

--- app/schemas/product_schema.py
+++ app/schemas/product_schema.py
@@ -1,21 +1,21 @@
-from pydantic import BaseModel
-from typing import Optional
+# from pydantic import BaseModel
+# from typing import Optional
 
-class ProductSchema(BaseModel):
-    id: int
-    name: str
-    price: float
-    description: str
+# class ProductSchema(BaseModel):
+#     id: int
+#     name: str
+#     price: float
+#     description: str
 
-    class Config:
-        from_attributes = True
+#     class Config:
+#         from_attributes = True
 
-class CreateProductSchema(BaseModel):
-    name: str
-    price: float
-    description: str
+# class CreateProductSchema(BaseModel):
+#     name: str
+#     price: float
+#     description: str
 
-class UpdateProductSchema(BaseModel):
-    name: Optional[str] = None
-    price: Optional[float] = None
-    description: Optional[str] = None
+# class UpdateProductSchema(BaseModel):
+#     name: Optional[str] = None
+#     price: Optional[float] = None
+#     description: Optional[str] = None
