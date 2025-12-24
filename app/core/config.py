e7ed636e1667e83bb31380ca6247e8fc33e5894f
Thanh Hieu <thanhhieu8304@gmail.com>
2025-12-24 23:13:14 +0700

    feat/hieupt: Setup MySQL configuration and customer model according to ERD

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,13 @@
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# class Settings:
-#     SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL")
+class Settings:
+    MYSQL_HOST: str = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_PORT: int = int(os.getenv("MYSQL_PORT", 3306))
+    MYSQL_USERNAME: str = os.getenv("MYSQL_USERNAME", "root")
+    MYSQL_PASSWORD: str = os.getenv("MYSQL_PASSWORD", "password")
+    DATABASE_NAME: str = os.getenv("DATABASE_NAME", "defaultdb")
+    DATABASE_URL: str = f"mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{DATABASE_NAME}"
 
-# settings = Settings()
+settings = Settings()
---
+++ app/models/customer_model.py
@@ -0,0 +1,23 @@
+from app.models.base_model import BaseModel
+from sqlalchemy import Integer, String, DateTime, Boolean, Enum
+from sqlalchemy.orm import Mapped, mapped_column, declared_attr
+from datetime import datetime
+import enum
+
+class Customer(BaseModel):
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return "customers"
+    
+    class Role(str, enum.Enum):
+        ADMIN = "admin"
+        CUSTOMER = "customer"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(length=100), index=True)
+    email: Mapped[str] = mapped_column(String(length=100), index=True)
+    password_hash: Mapped[str] = mapped_column(String(length=255))
+    role: Mapped[Role] = mapped_column(Enum(Role), default=Role.CUSTOMER)
+    created_at: Mapped[datetime] = mapped_column(DateTime, index=True, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    is_deactivated: Mapped[bool] = mapped_column(Boolean, default=False)
