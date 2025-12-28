a42e55c31318c2ec138838fb804292eca9ccc481
JJJ4065 <darknessrisesgamer999@gmail.com>
2025-12-28 17:28:09 +0700

    Resloved conflict

--- app/routers/order_router.py
+++ app/routers/order_router.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends
 from app.db.base import get_db
 from sqlalchemy.orm import Session
+from app.models.customer_model import Customer
 from app.models.order_model import Order
 from app.schemas.order_schema import OrderSchema, CreateOrderDetailSchema, UpdateOrderDetailSchema
 from app.schemas.base_schema import DataResponse
+from app.utils import send_email
+import os
 
 router = APIRouter()
 
@@ -18,6 +21,17 @@ async def create_order(data: CreateOrderDetailSchema, db: Session = Depends(get_
 	db.add(db_order)
 	db.commit()
 	db.refresh(db_order)
+	customer = db.query(Customer).filter(Customer.id == db_order.customer_id).first()
+	customer_email = customer.email if customer else "unknown@example.com"
+	# send mail to customer
+	template_path = os.path.join(os.path.dirname(__file__), "..", "templates", "order_confirmation_mail.html")
+	send_email(
+		template=template_path,
+		body={"order": db_order, "customer": customer},
+		subject="Thư xác nhận đơn đặt hàng",
+		retries=3,
+		smtp_user=customer_email
+	)
 	return DataResponse.custom_response(code="201", message="Created order", data=db_order)
 
 @router.get("/orders/{order_id}", tags=["orders"], description="Get an order by id", response_model=DataResponse[OrderSchema])
